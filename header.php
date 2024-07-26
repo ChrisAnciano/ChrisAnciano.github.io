23eb6482eab947c002f2a35686f32ff504a04d25
ChrisAnciano <Chris.anciano10@gmail.com>
2024-07-25 23:36:28 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Christopher Anciano's Portfolio</title>
+        <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+    <!-- Header Section -->
+    <header>
+        <div class="container">
+            <!-- Logo Section -->
+            <div class="logo">
+                <!-- Header text/logo -->
+                <h1> Christopher Anciano's Portfolio </h1>
+</div>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- 7/22/2024 Christopher Anciano
+  
+-->
+
+<!-- This is a declaration to lets us be compliant with HTML standards abd allowing us to use HTML tags and such -->
+<!DOCTYPE html>
+
+<!--The "<html>" tags represents the roots of the HTML document -->
+<html>
+
+
+<!--"<head>" tags represent a collection of metadata for the document
+    "<title>" tags are for setting the title of the website
+    "<body>" tags are for displaying the contents of the website
+    "<h>" tags are the headings of the website. "<h1>" has the largest font then it goes down in order
+    "<p>" tags represent a paragraph
+    "<br" tag is a line break
+    "<hr>" tag is a horizaontal rule which adds a line on the website
+    
+    Text formating
+    <b> is for bold text
+    <i> italic text
+    <big> is for big text
+    <small> for small text
+    <sub> for subscript text
+    <sup> for superscript text
+    <ins> for inserted(underlined) text
+    <del> for deleted(crossed out word) text
+    <mark> for highlighted text
+-->
+
+<head>
+    <h1> Christopher Anciano's Portfolio</h1>
+</head>
+
+</html>
